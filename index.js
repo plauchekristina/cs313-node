f782c1fd4b19520f5f508f7f69c9ccbf2119f07f
kripl <kristinaplauche@gmail.com>
2018-11-16 13:59:33 -0500

    beginning files

--- index.js
+++ index.js
@@ -7,4 +7,43 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/math', math)
+
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+
+function math(req, res) {
+  var operation = req.query.operation
+  var leftparam = Number(req.query.leftparam)
+  var rightparam = Number(req.query.rightparam)
+  doMath(res, operation, leftparam, rightparam)
+}
+
+function doMath(res, operation, leftparam, rightparam) {
+  var answer = 0;
+  switch (operation) {
+    case "+":
+      answer = leftparam + rightparam
+      break;
+    case "-":
+      answer = leftparam = rightparam
+      break;
+    case "*":
+      answer = leftparam * rightparam;
+      break;
+    case "/":
+      answer = leftparam / rightparam;
+      break;
+
+  }
+  var equation = {
+    operation: operation,
+    leftparam: leftparam,
+    rightparam: rightparam,
+    answer: answer
+  }
+  res.render('pages/result', equation);
+
+
+}
