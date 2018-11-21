b715c20120ffe74053cd070a77dc80bdfb5ac5a3
kripl <kristinaplauche@gmail.com>
2018-11-20 19:58:53 -0500

    fixing math function that was working

--- index.js
+++ index.js
@@ -24,59 +24,6 @@ function math(req, res) {
 function doMath(res, operation, leftparam, rightparam) {
   var answer = 0;
   switch (operation) {
-    case "stamped":
-      answer = (((leftparam * .21) - .21) + .50)
-      break;
-    case "metered":
-      answer = (((leftparam * .21) - .21) + .47)
-      break;
-    case "flats":
-      answer = (((leftparam * .21) - .21) + 1)
-      break;
-    case "retail":
-      answer = (((leftparam * .25) - .25) + 3.50)
-      break;
-
-  }
-  var equation = {
-    operation: operation,
-    leftparam: leftparam,
-    rightparam: rightparam,
-    answer: answer
-  }
-  res.render('pages/result', equation);
-
-
-}
-
-
-/* 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/math', math)
-  .get('/postal', (req, res) => res.sendFile(path.join(__dirname + '/public/submission.html')))
-
-  .listen(PORT, () => console.log(`Listening on ${PORT}`)) */
-
-
-/* 
-function math(req, res) {
-  var operation = req.query.operation
-  var leftparam = Number(req.query.leftparam)
-  var rightparam = Number(req.query.rightparam)
-  doMath(res, operation, leftparam, rightparam)
-}
-
-function doMath(res, operation, leftparam, rightparam) {
-  var answer = 0;
-  switch (operation) {
     case "+":
       answer = leftparam + rightparam
       break;
@@ -100,4 +47,4 @@ function doMath(res, operation, leftparam, rightparam) {
   res.render('pages/result', equation);
 
 
-} */
+} 
